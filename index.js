f32d4ebe0e3c0f313706419f15c447a20e939702
Abbeythetechguy <abbeyezeilo323@gmail.com>
2023-08-27 16:30:18 +0100

    Modified changes

--- index.js
+++ index.js
@@ -21,11 +21,8 @@ function closeSearchBar(){
 
 
 // to grab Marketplace for dropdown
-let marketplaceDropdown = document.getElementById("marketplace-dropdown");
-// marketplaceDropdown.addEventListener("click", showMarketPlaceMenu);
 
 function showMarketPlaceMenu(){
-    console.log("clicked")
     let showMarketdropdownElem = document.getElementById("marketplace-dropdown-content");
     showMarketdropdownElem.classList.toggle("showmarket-dropdown")
 }
@@ -35,9 +32,47 @@ function showSideNav(){
     let showMobNav = document.getElementById("mobile-menu");
     showMobNav.style.display = "block";
 }
+// for  closing mobile side nav
 function closeSideNavBar(){
     let hideSearchbar = document.getElementById("mobile-menu");
     hideSearchbar.style.display = "none";
 }
 
+// Mobile menu marketplace drop down
+function showMobileMarketDropdown(){
+    let showMobileMarketElem = document.getElementById("mobile-marketplace-dropdown-content");
+    showMobileMarketElem.classList.toggle("showMobmarket-dropdown")
+}
 
+// This is for the active tab
+function activeTab(){
+    const activeElem = [document.querySelectorAll("home"), document.querySelectorAll("all-nfts")];
+    activeElem.classList.add('.active-tab');
+}
+
+// function openCity(evt, cityName) {
+//     var i, x, tablinks;
+//     x = document.getElementsByClassName("nav-link");
+//     for (i = 0; i < x.length; i++) {
+//       x[i].style.display = "none";
+//     }
+//     tablinks = document.getElementsByClassName("nav-link");
+//     for (i = 0; i < x.length; i++) {
+//       tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+//     }
+//     document.getElementById(nav-link).style.display = "block";
+//     evt.currentTarget.className += " w3-red";
+//   }
+
+
+// function setActive(id) {
+//     document.getElementById('nav-link').classList.remove(`activeTab`);
+//     document.getElementById(id).classList.add(`activeTab`);
+//     const tab = document.getElementById('tab')
+//     tab.classList.remove(`activeTabhome`);
+//     tab.classList.remove(`activeTabcontact`);
+//     tab.classList.remove(`activeTabgallery`);
+//     tab.classList.remove(`activeTablogin`);
+//     tab.classList.add(`activeTab${id}`);
+
+// }
