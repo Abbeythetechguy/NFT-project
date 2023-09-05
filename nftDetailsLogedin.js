091020eb5a519e6dfb992adf4a4c741aca11c565
Faith dennis <dennisoghenekome10@gmail.com>
2023-09-05 14:11:15 +0100

    add new files

--- nftDetailsLogedin.js
+++ nftDetailsLogedin.js
@@ -1,12 +1,32 @@
-let a = document.getElementById(toggle);
-function toggle() {
-    if(a==1) {
-        document.getElementById(slider).style.display="inline";
-        return a=0;
-    }
-    else 
-    {
-        document.getElementById(slider).style.display="none";
-        return a=1;
-    }
+
+let priceBtnOne = document.getElementById ( "priceBtnOne");
+priceBtnOne.addEventListener ("click", showForm);
+
+function showForm () {
+    let overlayElem = document.getElementById ("overlayOne");
+    overlayElem.style.display = "block";
 }
+
+// let priceBtnTwo = document.getElementById ( "priceBtnTwo");
+// priceBtnTwo.addEventListener ("click", showForm);
+
+// function showForm () {
+//     let overlay = document.getElementById ("overlayTwo");
+//     overlay.style.display = "block";
+// }
+
+
+
+
+// let a = document.getElementById(toggle);
+// function toggle() {
+//     if(a==1) {
+//         document.getElementById(slider).style.display="inline";
+//         return a=0;
+//     }
+//     else 
+//     {
+//         document.getElementById(slider).style.display="none";
+//         return a=1;
+//     }
+// }
