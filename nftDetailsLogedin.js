45cb0f38af0714a8a849765b0c93e08f2d3d20d4
Faith dennis <dennisoghenekome10@gmail.com>
2023-09-08 14:48:59 +0100

    add

--- nftDetailsLogedin.js
+++ nftDetailsLogedin.js
@@ -34,9 +34,11 @@ let displayBtn = document.getElementById ( "displayBtn");
 displayBtn.addEventListener ("click", showHidden);
 
 function showHidden() {
-    let sliderElem = document.getElementById ("slider");sliderElem.classList.toggle("displayBtn");
-    sliderElem.style.overflow = "auto";
-    sliderElem.style.justifyContent = "space-evenly";
-    sliderElem.style.margin = "16rem 100rem";
-    sliderElem.style.flexWrap = "wrap";
+    let sliderElem = document.getElementById ("slider");
+    sliderElem.classList.toggle("btn");
+    console.log("showHidden");
+    // sliderElem.style.overflow = "auto";
+    // sliderElem.style.justifyContent = "space-evenly";
+    // sliderElem.style.margin = "16rem 100rem";
+    // sliderElem.style.flexWrap = "wrap";
 }
